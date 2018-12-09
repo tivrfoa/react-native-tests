524a68763863cc49ccf81dc5242b6f4ea8182915
heyjo <heyjo@notvalid.com>
2018-12-09 21:23:46 -0200

    touchables onLongPress

--- flex/App.js
+++ flex/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PizzaTranslator from './PizzaTranslator';
 import Flex from './flex1';
+import Touchables from './touchables';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PizzaTranslator />
+      <View>
+        <PizzaTranslator />
+        <Touchables />
+      </View>
     );
   }
 }
--- flex/PizzaTranslator.js
+++ flex/PizzaTranslator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class PizzaTranslator extends React.Component {
     constructor(props) {
@@ -9,19 +9,54 @@ export default class PizzaTranslator extends React.Component {
         };
     }
 
+    _onPressButton() {
+        Alert.alert('You tapped me.');
+    }
+
+    _dummyButton() {
+        Alert.alert('TODO');
+    }
+
     render() {
         return (
             <View style={{paddingTop: 40}}>
-                <TextInput
-                    style={{height: 40, fontSize: 30}}
-                    placeholder="Type here to translate!"
-                    onChangeText={(text) => this.setState({text})} />
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {/* {this.state.text + 'zzz'} */}
-                    {this.state.text.split(' ').
-                        map((word) => word && '🍕').join(' ')}
-                </Text>
+                <View>
+                    <TextInput
+                        style={{height: 40, fontSize: 30}}
+                        placeholder="Type here to translate!"
+                        onChangeText={(text) => this.setState({text})} />
+                    <Text style={{padding: 10, fontSize: 42}}>
+                        {/* {this.state.text + 'zzz'} */}
+                        {this.state.text.split(' ').
+                            map((word) => word && '🍕').join(' ')}
+                    </Text>
+                </View>
+                <View>
+                    <Button
+                        onPress={() => {
+                            Alert.alert('You tapped the button!');
+                        }}
+                        title="Press me"
+                    />
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Second Button"
+                        color="#841584"
+                    />
+                </View>
+                <View style={styles.rowLayout}>
+                    <Button onPress={this._dummyButton} title="CANCELAR" />
+                    <Button onPress={this._dummyButton} title="CONFIRMAR" />
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    rowLayout: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 20,
+    }
+});
